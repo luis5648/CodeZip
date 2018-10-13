9a0739cba89411b17decf7afeaf00b70ae00bcf7
Luis Román <porta5648@gmail.com>
2018-10-13 06:06:46 -0500

    Dirección del login al registro. registro inconcluso hasta nuevo aviso

---
+++ proyecto/Control(php_files)/regAlumnos.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "test";
+$password = "123";
+$dbname = "web";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+if (isset($_POST['registroAl'])) {
+  // code...
+}
+
+?>
