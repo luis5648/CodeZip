745de5ab6dfcd98cf1cbfad7210e8ecb8ad9e631
jl077 <jldg.077@gmail.com>
2018-10-19 01:10:46 -0500

    Line- 46 to 60 to file'JS/js-login.js' HELP ME ! , :v

--- proyecto/JS/js-login.js
+++ proyecto/JS/js-login.js
@@ -1,5 +1,5 @@
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
+
   var $this = $(this),
       label = $this.prev('label');
 
@@ -11,15 +11,15 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
         }
     } else if (e.type === 'blur') {
     	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
+    		label.removeClass('active highlight');
 			} else {
-		    label.removeClass('highlight');   
-			}   
+		    label.removeClass('highlight');
+			}
     } else if (e.type === 'focus') {
-      
+
       if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
+    		label.removeClass('highlight');
+			}
       else if( $this.val() !== '' ) {
 		    label.addClass('highlight');
 			}
@@ -28,16 +28,33 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 });
 
 $('.tab a').on('click', function (e) {
-  
+
   e.preventDefault();
-  
+
   $(this).parent().addClass('active');
   $(this).parent().siblings().removeClass('active');
-  
+
   target = $(this).attr('href');
 
   $('.tab-content > div').not(target).hide();
-  
+
   $(target).fadeIn(600);
-  
+
 });
+
+
+function validarMail(){
+  var mailUGD;
+  var mailNew;
+  var MailOk;
+  mailUGD="^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@"+"alumnos.udg.mx";
+  mailNew.getElementById("newMailRegistro");
+
+  if (mailUGD==mailNew) {
+      MailOk=1;
+  }
+  else {
+    MailOk=0;
+  }
+
+}
