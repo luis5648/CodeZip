04212d4621be6329217eafcc3cdc34a1bb743369
jl077 <jldg.077@gmail.com>
2018-10-26 21:20:36 -0500

    Arturo's commit

---
+++ proyecto/Control(php_files)/User.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+</head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../CSS/style-AboutUs.css">
+<script type="text/javascript" src="../JS/js-AboutUs.js"></script>
+<body>
+
+<?php
+
+	if ($conexión = mysql_connect("localhost", "root", "clave")){echo "";
+    $consulta= "SELECT*FROM mensajes";
+    mysql_select_db("web");$datos= mysql_query ($consulta);
+
+
+	<div id="wrapper">
+		<header id="js-header">
+			<div class="container clearfix">
+				<h1 id="logo">CODE.zip</h1>
+				<nav>
+					<a href="home.html">Inicio</a>
+					<a href="Cursos/MainCursos.html">Cursos</a>
+					<a href="#HOME">Acerca de</a>
+				</nav>
+			</div>
+		</header>
+
+
+		<br><br><br>
+		<div id="main">
+			<div id="content">
+					<div class="container">
+					<h1>Perfil de Usuario</h1>
+					<div align="left"><img src="profilePicture.png" width="100" height="100"></div>
+					Email de usuario: echo $fila ["emailAlumno"];
+					<br>
+					Nombre: echo $fila["nombreAlumno"];
+					<br>
+					Apellidos: echo $fila["apellidosAlumno"];
+					<br>
+					Nivel: Apellidos: echo $fila["nivel"];
+					<br>
+					</div>
+			</div>
+		</div>
+	}else{echo "<p> Error</p>";}?>
+	
+		<footer>
+			<div class="container clearfix">
+				<div class ="col" id="col-1">
+					<ul>
+						<li>CODE.zip/Route</li>
+						<li><a href="home.html">Home</a></li>
+
+						<li><a href="javascript:mensajeContact();">Contacto</a></li>
+					</ul>
+				</div>
+				<div  class ="col" id="col-2">
+					<ul>
+						<li>SOCIAL</li>
+						<li><a href="https://www.facebook.com/profile.php?id=100012609097340">Facebook</a></li>
+						<li><a href="javascript:mensajeTwitt();">Twitter</a></li>
+						<li><a href="#0">Youtube</a></li>
+					</ul>
+				</div>
+				<div  class ="col" id="col-3">
+					<ul>
+						<li>SERVICIOS</li>
+						<li><a href="javascript:mensajeEmergente();">Ejemplos</a></li>
+						<li><a href="javascript:mensajeEmergente();">Tutores</a></li>
+						<li><a href="javascript:mensajeEmergente();">Problemas</a></li>
+						<li><a href="javascript:mensajeEmergente();">Proyectos</a></li>
+					</ul>
+				</div>
+				<div id="copy">&#xa9; Universidad de Guadalajara, Centro Universitario de Tonalá <a href=""> ICCO</a>, Noviembre 2018</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
