8d12d1a714cd8e32bae9c022bd3435ff7cb877f9
jl077 <jldg.077@gmail.com>
2018-11-03 03:19:51 -0600

    Ya contamos con Logo, y algunas otras funciones nuevas, como efectos en la pag User y demás, se encuentra en desarrollo.

--- proyecto/Control(php_files)/User.php
+++ proyecto/Control(php_files)/User.php
@@ -1,81 +1,106 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profile</title>
+	<title>PERFIL USUARIO</title>
 </head>
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="../CSS/style-AboutUs.css">
-<script type="text/javascript" src="../JS/js-AboutUs.js"></script>
+<link rel="stylesheet" type="text/css" href="../CSS/style-perfilUser.css">
+<script type="text/javascript" src="../JS/js-UserPerfil.js"></script>
 <body>
 
-
-
-
-	<div id="wrapper">
+<div id="wrapper">
+<br>
+	
+<div>
 		<header id="js-header">
-			<div class="container clearfix">
-				<h1 id="logo">CODE.zip</h1>
-				<nav>
-					<a href="home.html">Inicio</a>
-					<a href="Cursos/MainCursos.html">Cursos</a>
-					<a href="#HOME">Acerca de</a>
-				</nav>
+			<div class="container clearfix"><br>
+			<img src="../PIC/01code.jpeg" alt="" class="img-Logo">	
+			<h1 id="logo">CODE.zip</h1>	
 			</div>
 		</header>
+</div>	
 
+<br><br><br><br>
+<form action="../Control(php_files)/cerrar.php">
+     <button type="submit" class="exit"name="botonSalir1">SALIR</button>
+    </form>
+<div class="tab">
+  <button class="tablinks" onclick="abrirContenido(event, 'perfilUsuario')">Perfil</button>
+  <button class="tablinks" onclick="abrirContenido(event, 'cursosUsuario')">Cursos</button>
+  <button class="tablinks" onclick="abrirContenido(event, 'material')">Material</button>
+</div>
 
-		<br><br><br>
-		<div id="main">
-			<div id="content">
-					<div class="container">
-					<h1>Perfil de Usuario</h1>
-					<div align="left"><img src="profilePicture.png" width="100" height="100"></div>
-                        <?php
-                        require 'conexion.php';
-                        session_start();
+<br><br>
 
-                        $user = $_SESSION['usuario'];
-                        $sql = "SELECT nombreAlumno,emailAlumno,apellidosAlumno
-                        from alumnos  where nombreUsuario = '$user'";
+<div class="main">
+	<div class="content">
 
-                        $result = $conn->query($sql);
-                        $row = $result->fetch_assoc();
-                        ?>
+					<div id="perfilUsuario" class="tabcontent">
+							<div class="container">
+								<h3>Perfil de usuario</h3>
 
-                        <p>Nombre del alumno: <?php echo $row["nombreAlumno"];?>  </p>
+								<div align="left"><img src="../PIC/user.png" width="100" height="100"></div>
+								<?php
+								require 'conexion.php';
+								session_start();
 
-					    <br>
+								$user = $_SESSION['usuario'];
+								$sql = "SELECT nombreAlumno,emailAlumno,apellidosAlumno
+								from alumnos  where nombreUsuario = '$user'";
 
-                        <p>E-mail registrado: <?php echo $row["emailAlumno"];?> </p>
-
-					    <br>
-
-					    <p>Apellidos: <?php echo $row["apellidosAlumno"];?>  </p>
-
-					    <br>
-					    <br>
+								$result = $conn->query($sql);
+								$row = $result->fetch_assoc();
+								?>
+								<p>Nombre del alumno: <?php echo $row["nombreAlumno"];?>  </p>
+								<p>E-mail registrado: <?php echo $row["emailAlumno"];?> </p>
+								<p>Apellidos: <?php echo $row["apellidosAlumno"];?>  </p>
+							</div>  
 					</div>
-			</div>
-		</div>
-		<footer>
-			<div class="container clearfix">
-				<div class ="col" id="col-1">
-					<ul>
+
+
+
+					<div id="cursosUsuario" class="tabcontent">
+						<div class="container">
+						<h3>Cursos disponibles</h3>
+						</div>
+					</div>
+
+
+
+					<div id="material" class="tabcontent">
+						
+						<div class="container">
+						<h3>Material academico disponible</h3>
+						</div>
+					
+					</div>
+
+	</div> <!--DIV CONTENT -->
+
+</div><!-- DIV MAIN-->
+
+
+<footer>
+	<div class="container clearfix">
+		<div class ="col" id="col-1">
+						<ul>
 						<li>CODE.zip/Route</li>
 						<li><a href="home.html">Home</a></li>
 
 						<li><a href="javascript:mensajeContact();">Contacto</a></li>
-					</ul>
-				</div>
-				<div  class ="col" id="col-2">
+						</ul>
+		</div>
+				
+		<div  class ="col" id="col-2">
 					<ul>
 						<li>SOCIAL</li>
 						<li><a href="https://www.facebook.com/profile.php?id=100012609097340">Facebook</a></li>
 						<li><a href="javascript:mensajeTwitt();">Twitter</a></li>
 						<li><a href="#0">Youtube</a></li>
 					</ul>
-				</div>
-				<div  class ="col" id="col-3">
+		</div>
+				
+		<div  class ="col" id="col-3">
 					<ul>
 						<li>SERVICIOS</li>
 						<li><a href="javascript:mensajeEmergente();">Ejemplos</a></li>
@@ -83,10 +108,12 @@
 						<li><a href="javascript:mensajeEmergente();">Problemas</a></li>
 						<li><a href="javascript:mensajeEmergente();">Proyectos</a></li>
 					</ul>
-				</div>
-				<div id="copy">&#xa9; Universidad de Guadalajara, Centro Universitario de Tonalá <a href=""> ICCO</a>, Noviembre 2018</div>
-			</div>
-		</footer>
+		</div>
+			<div id="copy">&#xa9; Universidad de Guadalajara, Centro Universitario de Tonalá <a href=""> ICCO</a>, Noviembre 2018</div>
 	</div>
+</footer>
+
+
+</div>
 </body>
 </html>
