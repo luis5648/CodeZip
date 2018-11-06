0a8bed319ce2e596e618f24f319803d05f31362b
Luis Román <porta5648@gmail.com>
2018-11-06 11:31:08 -0600

    creación de carpeta personalizada de soluciones segun usuario.

--- proyecto/Control(php_files)/subirProblemas.php
+++ proyecto/Control(php_files)/subirProblemas.php
@@ -1,24 +1,27 @@
 <?php
+session_start();
 
+$carpeta = $_SESSION['usuario'];
 
 $nombre=$_FILES['archivo']['name'];
 $guardado=$_FILES['archivo']['tmp_name'];
 
 
 //valida si existe la carpeta de los archivos
-if(!file_exists('Soluciones')){
+if(!file_exists('Soluciones/'."$carpeta")){
     //crea una carpeta con el nombre previamente dado
-    mkdir('Soluciones',0777,true);
-    if(file_exists('Soluciones')){
+    mkdir('Soluciones/'."$carpeta",0777,true);
+    if(file_exists('Soluciones/'."$carpeta")){
         //mueve el archivo a la ruta designada con el nombre original
-        if(move_uploaded_file($guardado, 'Soluciones/'."$nombre")){
-            echo "Archivo guardado con exito";
+        if(move_uploaded_file($guardado, 'Soluciones/'."$carpeta/"."$nombre")){
+            echo '<script type="text/javascript">alert("Archivo guardado.");
+    location.href="../PAGE-INIT/Cursos/Editores/Editor_C.php";</script>';
         }else{
             echo "Archivo no se pudo guardar";
         }
     }
 }else{
-    if(move_uploaded_file($guardado, 'Soluciones/'."$nombre")){
+    if(move_uploaded_file($guardado, 'Soluciones/'."$carpeta/"."$nombre")){
         echo '<script type="text/javascript">alert("Archivo guardado.");
     location.href="../PAGE-INIT/Cursos/Editores/Editor_C.php";</script>';
     }else{
