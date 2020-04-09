bc52e51131964573b136cd53cdb6b7bb15458c8b
Luis <porta5648@gmail.com>
2020-04-09 13:15:59 -0500

    Descargado de archivos completo (windows)

--- proyecto/princ/dw.php
+++ proyecto/princ/dw.php
@@ -1,5 +1,6 @@
 <?php
 
 header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.$_GET["file"].'"');
 
 echo file_get_contents('ftp://luis:newborn@localhost/' . $_GET["file"]);
